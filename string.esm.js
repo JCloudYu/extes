9a0e57e3253699daaf38f712f50a382234fd1230
Cloud Yu <j.cloud.yu@gmail.com>
2019-07-25 15:10:19 +0800

    Add String.lowerCase, String.localeLowerCase, String.camelCase methods

---
+++ string.esm.js
@@ -0,0 +1,29 @@
+/**
+ *	Author: JCloudYu
+ *	Create: 2019/07/25
+**/
+const CAMEL_CASE_PATTERN = /(\w)(\w*)(\W*)/g;
+const CAMEL_REPLACER = (match, $1, $2, $3, index, input )=>{
+	return `${$1.toUpperCase()}${$2.toLowerCase()}${$3}`;
+};
+
+Object.defineProperties(String, {
+	lowerCase:{
+		get:function() {
+			return this.toLowerCase();
+		}, configurable:false, enumerable:false
+	},
+	localeLowerCase:{
+		get:function() {
+			return this.toLocaleLowerCase();
+		}, configurable:false, enumerable:false
+	},
+	camelCase: {
+		get:function() {
+			return this.replace(CAMEL_CASE_PATTERN, CAMEL_REPLACER);
+		}, configurable:false, enumerable:false
+	}
+});
+
+
+
