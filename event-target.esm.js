58fae400dfa22c9916c4630919ac5b7f5b34d920
Cloud Yu <j.cloud.yu@gmail.com>
2019-12-07 17:47:03 +0800

    Let EventTarget::on api to accept comma separated event names each of which may consist of human readable tags.

--- event-target.esm.js
+++ event-target.esm.js
@@ -8,7 +8,17 @@ if ( typeof EventTarget !== "undefined" ) {
 	Object.defineProperty(EventTarget.prototype, 'on', {
 		configurable, writable, enumerable,
 		value: function(event_name, callback) {
-			this.addEventListener(event_name, callback);
+			// Event name accepts name1#tag1,name2#tag1,name3#tag2
+			const inserted = [];
+			const events = event_name.split(',');
+			for( let evt_name of events ) {
+				[evt_name] = evt_name.split('#');
+				evt_name = evt_name.trim();
+				if ( inserted.indexOf(evt_name) >= 0 ) continue;
+				
+				inserted.push(evt_name);
+				this.addEventListener(evt_name, callback);
+			}
 			return this;
 		}
 	});
